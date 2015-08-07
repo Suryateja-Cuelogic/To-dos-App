c5fb983bd1cfddef67cc22ff28e1a42d4cb67b78
SuryaTeja <suryateja.mahidhara@cuelogic.co.in>
2015-08-07 17:35:58 +0530

    Installed Mongoose module to interact with mongodb, Created a connection to mongodb with mongoose

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-var hapi = require('hapi');
+// Integrate HAPI and Mongoose Module.
+var hapi = require('hapi'),
+    mongoose = require('mongoose');
 
+// Create a server object, Establish a server connection.
 var server = new hapi.Server();
 
 server.connection({ port:2000 });
 
+// Connect mongoDB with mongoose.
+mongoose.connect("mongodb://localhost/to-dos");
+mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
+mongoose.connection.once('open', function () {
+	console.log("Connection to mongoDB is successful");
+});
+
+// Start the server.
 server.start(function () {
 	console.log("Server running at: ", server.info.uri );
 });
